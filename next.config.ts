aafa202fca5cdf6f23189f61d4536756b613d71d
Larry Finol <69995390+larrysfinolb@users.noreply.github.com>
2025-08-14 18:31:12 -0400

    🚑️ not load images

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     domains: ["prod-files-secure.s3.us-west-2.amazonaws.com"],
-    unoptimized: false
+    unoptimized: true
   },
   /* config options here */
 };
